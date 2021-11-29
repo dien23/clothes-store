a636aaf3e953a7737f93439afdcb179aa75aa016
Vo Thanh Dien <vothanhdien23520@gmail.com>
2021-11-29 12:35:57 +0700

    up

--- js/main.js
+++ js/main.js
@@ -1,2 +1,27 @@
-// animation
-AOS.init();
+// quantity
+const myInput = document.getElementById("my-input");
+function stepper(btn) {
+  let id = btn.getAttribute("id");
+  let min = myInput.getAttribute("min");
+  let max = myInput.getAttribute("max");
+  let step = myInput.getAttribute("step");
+  let value = myInput.getAttribute("value");
+  let calcStep = id == "increment" ? step * 1 : step * -1;
+  let newValue = parseInt(value) + calcStep;
+
+  if (newValue >= min && newValue <= max) {
+    myInput.setAttribute("value", newValue);
+  }
+}
+
+// detail
+function detailPopup() {
+  var detailPopup = document.getElementById("modal");
+  detailPopup.classList.toggle("active");
+}
+
+// search
+function search() {
+  var search = document.getElementById("search");
+  search.classList.toggle("active");
+}
